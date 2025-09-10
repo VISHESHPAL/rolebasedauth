4b5a8d366b4ec29dd3377893de935f4224350402
VISHESHPAL <156537104+VISHESHPAL@users.noreply.github.com>
2025-09-10 15:14:30 +0530

    adding the api

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api", 
-  withCredentials: true, 
+  baseURL: process.env.NODE_ENV === "production"
+    ? "https://rolebasedauth-wp16.vercel.app/api"
+    : "http://localhost:4000/api",
+  withCredentials: true,
 });
 
 export default api;
